133cffa0900d66df37d7742f0207c7c6bf8bf445
T8RIN <oolbp@bk.ru>
2023-05-31 16:57:50 +0300

    scroll to main app added

--- app/src/main/java/com/waveslider/sample/MainActivity.kt
+++ app/src/main/java/com/waveslider/sample/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -106,7 +108,11 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     ) { paddingValues ->
-                        Column(Modifier.padding(paddingValues)) {
+                        Column(
+                            Modifier
+                                .padding(paddingValues)
+                                .verticalScroll(rememberScrollState())
+                        ) {
                             var sliderValue by remember { mutableStateOf(0.4F) }
                             WaveSlider(
                                 value = sliderValue,
